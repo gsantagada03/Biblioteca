7ce4c5717a84b427d153bb108c65b6069dfe20fd
Giuseppe Santagada <g.santagada03@gmail.com>
2024-03-19 12:38:58 +0100

    Biblioteca versione 0.2

--- biblioteca/Biblioteca.java
+++ biblioteca/Biblioteca.java
@@ -4,29 +4,40 @@ public class Biblioteca {
     Libro libro = new Libro("", 0, 0);
     ArrayList<Libro> libri = new ArrayList<>();
 
+
+    //metodo che aggiunge in libro in base all'input dell'utente
     public void aggiungiLibro(String librodaAggiungere, int copieDisponibili) {
+        //ciclo che controlla se il libro è già presente
         for (int i = 0; i < libri.size(); i++) {
             if (libro.getNome().equals(librodaAggiungere)) {
                 System.out.println("Libro già presente");
                 break;
             }
         }
+        //in caso non sia presente, aggiunge il libro
         libri.add(libro);
 
     }
 
+
+    //metodo che rimuove il libro in base all'input dell'utente
     public void rimuoviLibro(String nomeLibroDaRimuovere) {
+        //ciclo che controlla il libro in base al titolo, se trova il libro lo rimuove
         for (int i = 0; i < libri.size(); i++) {
             if (libro.getNome().contains(nomeLibroDaRimuovere)) {
                 libri.remove(i);
                 break;
             }
         }
+        //in caso non lo trovi, stampa un messaggio di errore
         System.out.println("il libro non è presente nella biblioteca");
     }
 
+    //metodo che presta il libro
     public void prestaLibro(String nomeLibro) {
+        //ciclo che controlla il libro in base al titolo
         for (int i = 0; i < libri.size(); i++) {
+            //se il libro è presente e le copie disponibili sono maggiori di 0, manda un messaggio di avvenuto prestito e modifica le copie disponibili e quelle in presito
             if(libro.getNome().equals(nomeLibro)){
                 if(libro.getCopieDisponibili()>0){
                     System.out.println("libro prestato con successo");
@@ -38,6 +49,7 @@ public class Biblioteca {
         System.out.println("libro non trovato");
     }
 
+    //metodo che restituisce il libro
     public void restituisciLibro(String nomeLibro){
         for(int i = 0; i < libri.size(); i++){
             if(libro.getNome().equals(nomeLibro)){
@@ -47,5 +59,12 @@ public class Biblioteca {
             }
         }
         System.out.println("libro non trovato");
+    }
+
+    //metodo che stampa tutti i libri presenti
+    public void stampaLibri(){
+        for(int i = 0; i < libri.size(); i++){
+            System.out.println(libri.get(i));
+        }
     }
 }
--- biblioteca/Main.java
+++ biblioteca/Main.java
@@ -2,6 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Libro libro = new Libro("Il decameron", 10, 20);
+        Libro libro2 = new Libro("L'iliade", 10, 0);
+        
+        Biblioteca biblioteca = new Biblioteca();
+        Scanner sc = new Scanner(System.in);
 
+        biblioteca.stampaLibri();
+
+        System.out.println("che operazione vuoi effettuare?");
+        String scelta = sc.nextLine();
+
+        if(scelta.equals("prestito")){
+            System.out.println("Inserisci il libro che vuoi in prestito");
+            String libroInPrestito = sc.nextLine();
+            biblioteca.prestaLibro(libroInPrestito);
+
+        }else if(scelta.equals("restituzione")){
+            System.out.println("Inserisci il libro che vuoi restituire");
+            String librodaRestituire = sc.nextLine();
+            biblioteca.restituisciLibro(librodaRestituire);
+
+        }else if(scelta.equals("aggiunta")){
+            System.out.println("inserisci il libro che vuoi aggiungere e le copie");
+            String librodaAggiungere = sc.nextLine();
+            int copieDaAggiungere = sc.nextInt();
+            biblioteca.aggiungiLibro(librodaAggiungere,copieDaAggiungere );
+
+        }else if(scelta.equals("rimozione")){
+            System.out.println("inserisci il libro che vuoi rimuovere");
+            String libroDaRimuovere = sc.nextLine();
+            biblioteca.rimuoviLibro(libroDaRimuovere);
+        }
+        sc.close();
     }
 }
